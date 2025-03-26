820e372233dfe25507d3841438a49e4c98c8184d
FionaRoberts007 <158515211+FionaRoberts007@users.noreply.github.com>
2025-03-26 10:47:45 -0700

    Add files via upload

---
+++ document.js
@@ -0,0 +1,16 @@
+// document.getElementById('contact-form').addEventListener('submit', function(event){
+//     event.preventDefault(); // Prevent the form from submitting via the browser
+    
+//     let name = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     let message = document.getElementById('message').value;
+
+//     // Simple validation - for demonstration purposes
+//     if (name && email && message) {
+//         alert("Thank you for your message!");
+//     } else {
+//         alert("Please fill out all fields.");
+//     }
+
+//     // Here, you'd typically include AJAX to send the form data to your server without reloading the page
+// });
